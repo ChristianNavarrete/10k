c54fb84b1256ad91ea0e8b5233768a53ddbfe0bb
HoodsDream <christiannavarette@hoodsdreams-mbp.charter.com>
2014-11-17 23:15:13 -0800

    added CoreData methods

---
+++ 10K/GoalEntry.h
@@ -0,0 +1,21 @@
+//
+//  GoalEntry.h
+//  10K
+//
+//  Created by HoodsDream on 11/17/14.
+//  Copyright (c) 2014 HoodsDream. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface GoalEntry : NSManagedObject
+
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSData * background;
+@property (nonatomic) float allTimeFloat;
+@property (nonatomic) float dailyFloat;
+@property (nonatomic) NSTimeInterval date;
+
+@end
---
+++ 10K/GoalEntry.m
@@ -0,0 +1,20 @@
+//
+//  GoalEntry.m
+//  10K
+//
+//  Created by HoodsDream on 11/17/14.
+//  Copyright (c) 2014 HoodsDream. All rights reserved.
+//
+
+#import "GoalEntry.h"
+
+
+@implementation GoalEntry
+
+@dynamic title;
+@dynamic background;
+@dynamic allTimeFloat;
+@dynamic dailyFloat;
+@dynamic date;
+
+@end
